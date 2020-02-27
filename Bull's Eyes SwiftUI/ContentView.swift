8877b810b13923d5dee088b3757ea2e5b42cd654
Alex Zerah <alex.zerah@gmail.com>
2020-02-27 09:52:24 +0100

    Initial Commit

---
+++ Bull's Eyes SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bull's Eyes SwiftUI
+//
+//  Created by alexzerah on 27/02/2020.
+//  Copyright © 2020 alexzerah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
